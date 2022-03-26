edc860626635ec29469feb2e30b9136cdff9abc2
MariaDanu <101257907+MariaDanu@users.noreply.github.com>
2022-03-26 17:13:59 +0500

    Add files via upload

--- Bun_p.cs
+++ Bun_p.cs
@@ -29,9 +29,35 @@ public class Bun_p : MonoBehaviour
         //_placed = true;
         if (!_dragging) return;
 
-        var mousePosition = (Vector2)Camera.main.ScreenToWorIdPoint(Input.mousePosition); //ошибка!!!
+        var mousePosition = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         transform.position = mousePosition - _offset;
     }
 
+    private void OnMouseDown()
+    {
+        _dragging = true;
+        _offset = GetMousePos() - (Vector2)transform.position;
+    }
+
+    private void OnMouseUp()
+    {
+        if(Vector2.Distance(transform.position, _pslot.transform.position) < 3)
+        {
+            transform.position = _pslot.transform.position;
+            _pslot.Placed();
+            _placed = true;
+        }
+        else
+        {
+            _dragging = false;
+            //_offset = _pslot.transform.position;
+            transform.position = _firstPosition;
+        }
+    }
+
+    Vector2 GetMousePos()
+    {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
 }
